8487cf5bfce9b03939231cc72b4145bd32f6c10a
cararoggemans <cara.roggemans@acolad.com>
2025-03-17 18:36:42 +0100

    change image import to fix build

--- src/components/MetaDescription.js
+++ src/components/MetaDescription.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom";
-import image from '../../public/cara-roggemans-allround-digital-creative.png'
 
 const MetaDescription = ({ title, description }) => {
   const { i18n } = useTranslation();
@@ -10,6 +9,7 @@ const MetaDescription = ({ title, description }) => {
   
   const currentLang = i18n.language;
   const url = `https://cararoggemans.com/${currentLang}${location.pathname}`;
+  const imageUrl = "https://cararoggemans.com/cara-roggemans-allround-digital-creative.png";
 
   return (
     <Helmet>
@@ -20,8 +20,8 @@ const MetaDescription = ({ title, description }) => {
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content={image} />
-      <meta property="og:image:secure_url" content={image} />
+      <meta property="og:image" content={imageUrl} />
+      <meta property="og:image:secure_url" content={imageUrl} />
       <meta property="og:image:alt" content="Cara Roggemans - Allround Digital Creative - graphic design, web design, social media and content creation" />
       <meta property="og:image:width" content="1367" />
       <meta property="og:image:height" content="627" />
@@ -30,7 +30,7 @@ const MetaDescription = ({ title, description }) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={imageUrl} />
       <meta name="twitter:image:alt" content="Cara Roggemans - Allround Digital Creative - graphic design, web design, social media and content creation" />
 
       <meta name="robots" content="index, follow" />
