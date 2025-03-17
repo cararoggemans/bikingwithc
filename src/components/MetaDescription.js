5f777e35889fe32723a8d676fa41819a49e08779
cararoggemans <cara.roggemans@acolad.com>
2025-03-17 18:31:51 +0100

    fix meta images

--- src/components/MetaDescription.js
+++ src/components/MetaDescription.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom";
-import image from '../img/meta/cara-roggemans-allround-digital-creative-min.png'
+import image from '../../public/cara-roggemans-allround-digital-creative.png'
 
 const MetaDescription = ({ title, description }) => {
   const { i18n } = useTranslation();
@@ -21,7 +21,8 @@ const MetaDescription = ({ title, description }) => {
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
       <meta property="og:image" content={image} />
-      <meta property="og:image:alt" content="Cara Roggemans - Allround Digital Creative" />
+      <meta property="og:image:secure_url" content={image} />
+      <meta property="og:image:alt" content="Cara Roggemans - Allround Digital Creative - graphic design, web design, social media and content creation" />
       <meta property="og:image:width" content="1367" />
       <meta property="og:image:height" content="627" />
 
@@ -30,7 +31,7 @@ const MetaDescription = ({ title, description }) => {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
-      <meta name="twitter:image:alt" content="Cara Roggemans - Allround Digital Creative" />
+      <meta name="twitter:image:alt" content="Cara Roggemans - Allround Digital Creative - graphic design, web design, social media and content creation" />
 
       <meta name="robots" content="index, follow" />
     </Helmet>
