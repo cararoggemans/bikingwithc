5ec259587d3f6dbbf0b8a1236d1144549b5f32fe
cararoggemans <cara.roggemans@acolad.com>
2025-09-10 13:14:14 +0200

    fix homepage, robots, sitemap and meta descriptons

--- src/components/MetaDescription.js
+++ src/components/MetaDescription.js
@@ -5,7 +5,7 @@ import { useLocation } from "react-router-dom";
 const MetaDescription = ({ title, description }) => {
   const location = useLocation();
 
-  const baseUrl = "https://cararoggemans.com";
+  const baseUrl = "https://bikingwithc.com";
   const currentPath = location.pathname;
   const url = `${baseUrl}${currentPath}`;
   const imageUrl = `${baseUrl}/cara-roggemans-allround-digital-creative-def.png`;
@@ -27,7 +27,7 @@ const MetaDescription = ({ title, description }) => {
       <meta property="og:type" content="website" />
       <meta property="og:image" content={imageUrl} />
       <meta property="og:image:secure_url" content={imageUrl} />
-      <meta property="og:image:alt" content="Cara Roggemans - Allround Digital Creative - graphic design, web design, social media and content creation" />
+      <meta property="og:image:alt" content="BikingWithC is a motorcycle blog by Cara, sharing personal stories, ride planning tips and inspiration for riders around the world." />
       <meta property="og:image:width" content="1367" />
       <meta property="og:image:height" content="627" />
 
@@ -36,7 +36,7 @@ const MetaDescription = ({ title, description }) => {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={imageUrl} />
-      <meta name="twitter:image:alt" content="Cara Roggemans - Allround Digital Creative - graphic design, web design, social media and content creation" />
+      <meta name="twitter:image:alt" content="BikingWithC is a motorcycle blog by Cara, sharing personal stories, ride planning tips and inspiration for riders around the world." />
 
       <meta name="robots" content="index, follow" />
     </Helmet>
